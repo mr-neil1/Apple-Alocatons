a5459d1f8d8f93fb98c0015df2d1c9dcc5835985
mr-neil1 <samyneil4@gmail.com>
2025-06-26 15:26:02 +0100

    Initial commit

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,17 @@
+// frontend/src/firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Ejaeyve6NrTHwc3kxivPJHFMWkwblZE",
+  authDomain: "apple-allocations.firebaseapp.com",
+  projectId: "apple-allocations",
+  storageBucket: "apple-allocations.firebasestorage.app",
+  messagingSenderId: "202039961909",
+  appId: "1:202039961909:web:74f017498005b2591c447c",
+  measurementId: "G-SNQ5JJ827J"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app); // Connexion à Firestore
